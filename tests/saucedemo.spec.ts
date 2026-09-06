a7da12935f2c8e4f081a4e27994eb156ebdf988d
Anbuselvan <114353805+anbu56546@users.noreply.github.com>
2026-09-06 17:19:42 +0530

    Add SauceDemo Playwright test

---
+++ tests/saucedemo.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Verify SauceDemo title and URL', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await expect(page).toHaveTitle('Swag Labs');
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
