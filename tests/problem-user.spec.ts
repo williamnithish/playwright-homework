277a50a087b91b889b6d14c7ba5929a943910147
Anbuselvan <114353805+anbu56546@users.noreply.github.com>
2026-09-20 14:55:29 +0530

    Add problem user project and session

---
+++ tests/problem-user.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('problem_user shows same image for all products', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/inventory.html');
+
+    const images = page.locator('.inventory_item_img img');
+
+    await expect(images).toHaveCount(6);
+
+    const firstImage = await images.nth(0).getAttribute('src');
+
+    for (let i = 1; i < 6; i++) {
+        await expect(images.nth(i)).toHaveAttribute('src', firstImage!);
+    }
+});
